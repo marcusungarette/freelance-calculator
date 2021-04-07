8da7b2a731208ba6784bda7036706c9676eb3d63
marcusungarette <marcusungarette@gmail.com>
2021-04-07 17:19:55 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const routes = require('./routes');
+
+const port = 3535;
+
+server.set('view engine', 'ejs');
+
+server.use(express.static('public'));
+
+server.use(routes);
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
